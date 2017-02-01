f4b90eea8e1c677606c32a3d78f708f07b536875
Right Hand Abacus <righthandabacus@users.noreply.github.com>
2017-01-31 19:04:36 -0500

    Update of helper module; add gitignore

--- phantomjs.py
+++ phantomjs.py
@@ -14,7 +14,7 @@ class phantomjs(object):
         })
         self.browser = webdriver.PhantomJS(desired_capabilities=DCAP)
         self.browser.set_window_size(width, height)
-        self.browser.implicitly_wait(10) # seconds
+        #self.browser.implicitly_wait(10) # seconds
 
     def __del__(self):
         import signal
@@ -26,3 +26,38 @@ class phantomjs(object):
 
     def wait_until_staled(self, element, timeout=30):
         WebDriverWait(self.browser, timeout).until(staleness_of(element))
+
+    def get_xpath(self, element):
+        js = '''
+            // adapted from http://stackoverflow.com/questions/2631820/im-storing-click-coordinates-in-my-db-and-then-reloading-them-later-and-showing/2631931#2631931
+            var getXPathTo = function (element){
+                /* -- if we allow id() in XPath
+                if (element.id !== '')
+                    return 'id("' + element.id + '")';
+                */
+                // Recursion breaker: at document root
+                if (element === document)
+                    return '';
+                // Recursively find path of this element by counting siblings
+                var count = 0;
+                var hit = 0;
+                var siblings = element.parentNode.childNodes;
+                for (var i=0; i<siblings.length; i++) {
+                    if (siblings[i].nodeType===1 && siblings[i].tagName===element.tagName)
+                        count++;
+                    if (siblings[i] === element)
+                        hit = count;
+                    if (count > 1 && hit >= 1)
+                        break;
+                };
+                // Return XPath
+                if (hit == 1 && count == 1) {
+                    return getXPathTo(element.parentNode)+'/'+element.tagName.toLowerCase();
+                } else {
+                    return getXPathTo(element.parentNode)+'/'+element.tagName.toLowerCase()+'['+hit+']';
+                };
+            };
+            return getXPathTo(arguments[0]);
+        '''
+        ret = self.browser.execute_script(js, element)
+        return ret
