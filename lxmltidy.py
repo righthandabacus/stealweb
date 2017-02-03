51004cd5f5bc37682b139ca6bafce3a776852f96
Right Hand Abacus <righthandabacus@users.noreply.github.com>
2017-02-03 15:15:52 -0500

    New tool: lxmltidy.py

---
+++ lxmltidy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from lxml import etree
+import sys
+print etree.tostring(
+        etree.fromstring(
+            open(sys.argv[1], 'rU').read(),
+            parser = etree.HTMLParser(remove_blank_text=True, remove_comments=True, remove_pis=True)
+        )
+        ,encoding='utf8', pretty_print=True, method='xml'
+    )
